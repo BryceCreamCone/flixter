d33127db5507c5d90ebe9001332773e03dd771cc
Bryce Gust <bgust9@gmail.com>
2019-08-21 16:12:01 +0000

    Added Instructor and Courses, as well as sticky footer path to add a new course if you are an instructor.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
+  has_many :courses
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable
+
+  
+
+  protected
+  def confirmation_required? 
+    false 
+  end
 end
