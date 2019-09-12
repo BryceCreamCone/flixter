5df620b681eb1d4536508ede57f3eae4145bbbe0
Bryce Gust <bgust9@gmail.com>
2019-09-12 17:18:30 +0000

    Added Enrollment table to connect users and courses they have enrolled in. Hooked up routes to allow for users to create an enrollment and enroll in a course. Styled courses page to show enroll button and tell if the user has already been enrolled. Logic in user.rb to check for enrollment in course.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,7 +9,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable
 
-  
+  def enrolled_in?(course)
+    return enrolled_courses.include?(course)
+  end
 
   protected
   def confirmation_required? 
