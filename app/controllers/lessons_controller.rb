0ae76d4d5250fa601bba1ac1d8f786fa9ce0dc49
Bryce Gust <bgust9@gmail.com>
2019-09-14 17:12:13 +0000

    Added logic to the lessons_controller to check if the user has enrolled before allowing them to continue to the Lessons page for a course.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,24 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    unless current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course),
+      alert: 'You have not purchased this course'
+    end
   end
 
   private
 
-  helper_method :current_lesson
+  helper_method :current_lesson, :current_course
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def current_course
+    current_section ||= Section.find_by(id: current_lesson.section_id)
+    @current_course ||= Course.find_by(id: current_section.course_id)
+  end
+
+  
 end
